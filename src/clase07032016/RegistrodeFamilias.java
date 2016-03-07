08ffdc022588fa09c511c5418d4cca445949980c
Estudiante <Estudiante@168.176.24.207>
2016-03-07 15:42:02 -0500

    clase 22022016

--- src/clase07032016/RegistrodeFamilias.java
+++ src/clase07032016/RegistrodeFamilias.java
@@ -4,19 +4,20 @@
  * and open the template in the editor.
  */
 package clase07032016;
-import java.util.*;
+
 /**
  *
  * @author Estudiante
  */
-public class TreeHash {
+public class RegistrodeFamilias {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Se estudará el Uso del TreeMap, de acuerdo al orden alfabetico el organiza, de tal manera que se hace mas facil ubicar los datos
-        HashMap <String, String> h = new HashMap <String, String>();
-        TreeMap <String, String> t = new TreeMap <String, String>();
+        
+        //Entregar esto para la proxima clase, y hace los diagramas del taller de modelamiento
+        
     }
+    
 }
